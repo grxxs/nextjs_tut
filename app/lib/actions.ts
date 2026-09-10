7d7cdffd3abb5bbf812db90d97792ce8015dc826
grxxs <ravakonline@gmail.com>
2026-09-10 11:09:56 +0200

    finished chapter p2 of chapter 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,6 +13,9 @@ import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
+import { signIn, signOut } from "@/auth";
+import { AuthError } from "next-auth";
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
@@ -111,4 +114,27 @@ export async function deleteInvoice(id: string) {
   `;
 
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
+
+export async function logOut() {
+  await signOut({ redirectTo: "/" });
 }
